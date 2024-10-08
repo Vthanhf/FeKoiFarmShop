5aaf6506993fea51d48ef40073d6a3ecaae1133b
Đỗ Khánh Huy <dokhanhhuy01@gmail.com>
2024-10-08 18:40:10 +0700

    config

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
 import Dashboard from "./pages/dashboard";
 import HomePage from "./pages/home/HomePage";
-
+import Header2 from "./components/main/Header2";
+import Rating from "./pages/Rating/Rating";
 import ProductDetail from "./pages/1Koi/ProductDetail";
-
+import PaymentPageOnline from "./pages/payment/PaymentOnl";
+import OrderStatus from "./pages/OrderStatus/OrderStatus";
 import Header from "./components/main/Header";
 import Footer from "./components/main/Footer";
 import ProductPage from "./pages/product/ProductPage";
-import Payment from "./pages/payment/payment";
+import Packs from "./pages/packs/packs";
 
 // import StudentManagement from "./StudentManagement";
 
@@ -42,9 +44,45 @@ function App() {
           path: "/detail",
           element: <ProductDetail />,
         },
+      ],
+    },
+    {
+      path: "",
+      element: (
+        <>
+          <Header2 />
+          <Outlet />
+          <Footer />
+        </>
+      ),
+      children: [
         {
-          path: "/payment",
-          element: <Payment />,
+          path: "/v",
+          element: <HomePage />,
+        },
+        {
+          path: "/productv",
+          element: <ProductPage />,
+        },
+        {
+          path: "/detailv",
+          element: <ProductDetail />,
+        },
+        {
+          path: "paymentonline",
+          element: <PaymentPageOnline />,
+        },
+        {
+          path: "orderstatus",
+          element: <OrderStatus />,
+        },
+        {
+          path: "rating",
+          element: <Rating />,
+        },
+        {
+          path: "package",
+          element: <Packs />,
         },
       ],
     },
@@ -67,22 +105,6 @@ function App() {
     {
       path: "dashboard",
       element: <Dashboard />,
-    },
-
-    {
-      path: "payment",
-      element: <Payment />,
-    },
-
-    {
-      path: "",
-      element: (
-        <>
-          <Header />
-          <ProductPage />
-          <Footer />
-        </>
-      ),
     },
   ]);
 
