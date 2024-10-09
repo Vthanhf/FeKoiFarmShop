5d6ff16e7e8af8f52f8671b5e16c2042d7b89746
Đỗ Khánh Huy <dokhanhhuy01@gmail.com>
2024-10-09 13:45:51 +0700

    payment offline

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Header from "./components/main/Header";
 import Footer from "./components/main/Footer";
 import ProductPage from "./pages/product/ProductPage";
 import Packs from "./pages/packs/packs";
+import Payment from "./pages/payment/index";
 
 // import StudentManagement from "./StudentManagement";
 
@@ -83,6 +84,10 @@ function App() {
         {
           path: "package",
           element: <Packs />,
+        },
+        {
+          path: "payment",
+          element: <Payment />,
         },
       ],
     },
--- src/pages/payment/index.jsx
+++ src/pages/payment/index.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import "./Payment.css";
+import "./index.css";
 
 function Payment() {
   return (
