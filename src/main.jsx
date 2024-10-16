ae246499d62340b4ca2bde02c2b7bbf0a3086331
Đỗ Khánh Huy <huydkse183811@fpt.edu.vn>
2024-10-16 16:08:23 +0700

    Add Redux to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import App from "./App.jsx"
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
 <>
+    <Provider store={store}>
+
     <App />
     <ToastContainer />
+    </Provider>    
 </>);
 
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,4 +6,4 @@ export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
-})
+});
